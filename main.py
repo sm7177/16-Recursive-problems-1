72d5ec785c1b07ee6c18c48b870590fe961717f7
sm7177 <subrinamaharjan2020@gmail.com>
2024-12-09 18:51:36 +0545

    .

--- main.py
+++ main.py
@@ -33,19 +33,57 @@
 
 
 
-n=int(input("Enter number:"))
+# n=int(input("Enter number:"))
 
-def checkpower(n):
-    if (n<=0):
-        return False
-    if (n==1):
-        return True
-    if (n%4==0):
-        return checkpower(n/4)
-    return False
+# def checkpower(n):
+#     if (n<=0):
+#         return False
+#     if (n==1):
+#         return True
+#     if (n%4==0):
+#         return checkpower(n/4)
+#     return False
 
-if (checkpower(n)):
-    print("It is power of 4.")
+# if (checkpower(n)):
+#     print("It is power of 4.")
 
-else:
-    print("It is not power of 4.")
+# else:
+#     print("It is not power of 4.")
+
+
+
+
+
+
+
+
+# def gcd(a,b):
+#     if b==0:
+#         return a 
+#     return gcd(b,a%b)
+
+
+# a=int(input("Enter a:"))
+
+# b=int(input("Enter b:"))
+# print(gcd(a,b))
+
+
+
+
+
+
+
+
+
+
+
+def power(x,n):
+    if n==0:
+        return 1
+    return x*power(x,n-1)
+    
+x=int(input("Enter number:"))
+n=int(input("Enter power:"))
+
+print(power(x,n))
